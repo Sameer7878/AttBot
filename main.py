aca0c2c2ea99c7cc6b52d6d8d9c83433a3225228
shaik sameer <95042238+Sameer7878@users.noreply.github.com>
2022-09-07 21:06:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2051,9 +2051,9 @@ while (True):
         if not username:
             username = get_username()
             print(username)
-        cur.execute(f"select count(*),active_status from instad where insta_username='{username}';")
-        status1=cur.fetchone()
-        status=status1[0]
+        cur.execute(f"select count(*)from instad where insta_username='{username}';")
+        status=cur.fetchone()
+        status=status[0]
         if msg and not(status):
             if msg in student_data:
                cur.execute(f"select count(*) from instad where rolid='{msg}'")
@@ -2098,8 +2098,9 @@ while (True):
                         conn.close()
                         read_unread_msgs()
                         continue
-                        
-        elif status1[1] and msg == 'START':
+        cur.execute(f"select active_status from instad where insta_username='{username}';")
+        status1=cur.fetchone()               
+        elif status1[0] and msg == 'START':
             cur.execute(f"update instad set active_status=true where insta_username='{username}'")
             conn.commit()
             send_msg('Status:Active')
@@ -2109,7 +2110,7 @@ while (True):
             conn.close()
             read_unread_msgs()
             continue
-        elif not status1[1]:
+        elif not status1[0]:
             username=None
             msg=None
             msg_count=0
